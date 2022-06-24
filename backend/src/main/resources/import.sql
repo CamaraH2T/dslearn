0e6d7ef053f5ff1d3b5ce08ace93d7fc2610c916
Heittor Câmara <heittor_camara@hotmail.com>
2022-06-23 21:41:21 -0300

    Offer

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -12,3 +12,7 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
+
+INSERT INTO tb_course (name, img_Uri, img_Gray_Uri) VALUES ('Bootcamp HTML', 'https://1.bp.blogspot.com/-hUe-a2BlvWU/V7NBLtE8NnI/AAAAAAAAH5s/F8UL76I8UeE0PQDlqJNDzSg-OGQS3bJEgCLcB/s1600/CURSOS.png', 'https://icones.pro/wp-content/uploads/2022/02/icone-formation-cours-conference-gris.png')
+
+INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('1.0', TIMESTAMP WITH TIME ZONE '2020-07-14T03:00:00Z', TIMESTAMP WITH TIME ZONE '2021-07-14T03:00:00Z', 1)
