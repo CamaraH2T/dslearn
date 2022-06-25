9a093f76fb90041e05af0704ed6faf19376e0df4
Heittor Câmara <heittor_camara@hotmail.com>
2022-06-25 18:27:47 -0300

    Resources

--- backend/src/main/java/com/devsuperior/dslearnbds/entities/Offer.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/entities/Offer.java
@@ -2,41 +2,49 @@ package com.devsuperior.dslearnbds.entities;
 
 import java.io.Serializable;
 import java.time.Instant;
-import java.util.Objects;
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "tb_offer")
 public class Offer implements Serializable {
-	
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;	
-	private String edtion;
+	private Long id;
+	private String edition;
+	
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
 	private Instant startMoment;
+
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
 	private Instant endMoment;
 	
 	@ManyToOne
 	@JoinColumn(name = "course_id")
 	private Course course;
 	
+	@OneToMany(mappedBy = "offer")
+	private List<Resource> resources = new ArrayList<>();
+	
 	public Offer() {
-		
 	}
 
-	public Offer(Long id, String edtion, Instant startMoment, Instant endMoment, Course course) {
+	public Offer(Long id, String edition, Instant startMoment, Instant endMoment, Course course) {
 		super();
 		this.id = id;
-		this.edtion = edtion;
+		this.edition = edition;
 		this.startMoment = startMoment;
 		this.endMoment = endMoment;
 		this.course = course;
@@ -50,12 +58,12 @@ public class Offer implements Serializable {
 		this.id = id;
 	}
 
-	public String getEdtion() {
-		return edtion;
+	public String getEdition() {
+		return edition;
 	}
 
-	public void setEdtion(String edtion) {
-		this.edtion = edtion;
+	public void setEdition(String edition) {
+		this.edition = edition;
 	}
 
 	public Instant getStartMoment() {
@@ -81,10 +89,17 @@ public class Offer implements Serializable {
 	public void setCourse(Course course) {
 		this.course = course;
 	}
+	
+	public List<Resource> getResources() {
+		return resources;
+	}
 
 	@Override
 	public int hashCode() {
-		return Objects.hash(id);
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
 	}
 
 	@Override
@@ -96,9 +111,11 @@ public class Offer implements Serializable {
 		if (getClass() != obj.getClass())
 			return false;
 		Offer other = (Offer) obj;
-		return Objects.equals(id, other.id);
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
 	}
-	
-	
-
 }
